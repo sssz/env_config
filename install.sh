5ad4d35ed29cf9d34151417de5ca2520b68e0acf
jing.zhou <jing.zhou@hulu.com>
2020-07-23 06:54:02 -0700

    update

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
+sudo apt-get install curl
+sudo apt-get install git
+sudo apt-get install vim
+
 #1, install zsh
-#sudp apt-get install zsh
+sudo apt-get install zsh
 sudo  curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
 git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 cp ./zshrc ~/.zshrc
-chsh -s /bin/bash
 
 
 #2, install tmux
@@ -12,7 +15,9 @@ sudo apt-get install tmux
 cp ./tmux.conf ~/.tmux.conf
 
 
-
 #3, bash jump to tmux
+cp ./bashrc ~/.bashrc
 
 
+#4, vim
+cp ./vimrc ~/.vimrc
---
+++ vimrc
@@ -0,0 +1,11 @@
+set nu
+set syntax=on
+set ts=4
+set expandtab
+%retab!
+
+
+
+set fileencodings=utf-8,gb2312,gb18030,gbk,ucs-bom,cp936,latin1
+set enc=utf8
+set fencs=utf8,gbk,gb2312,gb18030
