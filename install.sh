81dae967f1ee79633911657041c7d27fb76e5ac4
sssz <zjcf825@163.com>
2023-02-12 17:20:43 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ sudo apt-get install vim
 
 #1, install zsh
 sudo apt-get install zsh
-sudo  curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 cp ./robbyrussell.zsh-theme  ~/.oh-my-zsh/themes/robbyrussell.zsh-theme
